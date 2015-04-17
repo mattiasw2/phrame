8eeef054096f1558a6acc24955bf73359e3bb0b4
Hans Huebner <hans.huebner@gmail.com>
2015-04-17 06:16:30 +0200

    Fix typo

--- src/phrame/server.clj
+++ src/phrame/server.clj
@@ -1,7 +1,7 @@
 (ns phrame.server
   (:require [org.httpkit.server :as http-server]
             [compojure.core :as compojure]
-            [phram.config :refer [config]]
+            [phrame.config :refer [config]]
             [phrame client-handlers oauth-handlers]))
 
 (defonce server (atom nil))
