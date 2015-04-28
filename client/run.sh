34346a0a4a8580e5afa3eb5b14fd89f8c029577c
Hans Huebner <hans.huebner@gmail.com>
2015-04-28 19:59:57 +0000

    Make websocket URL into command line argument

--- client/run.sh
+++ client/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+PHRAME_URL=${1:-wss://netzhansa.com/websocket}
+
 while true
 do
-    sudo python client.py wss://netzhansa.com/websocket
+    sudo python client.py $PHRAME_URL
     echo client exited
     sleep 1
 done
