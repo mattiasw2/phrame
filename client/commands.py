622c4cb56a849a0d9d0b8e080e024173625bd4c6
Hans Huebner <hans.huebner@gmail.com>
2015-04-30 06:35:51 +0000

    Implement client power_up and power_down commands

--- client/commands.py
+++ client/commands.py
@@ -4,6 +4,7 @@ import pygame
 import time
 import os
 import sys
+import subprocess
 import io
 import math
 from urllib2 import urlopen
@@ -12,26 +13,22 @@ import json
 
 os.putenv('SDL_VIDEODRIVER', 'fbcon')
 
-pygame.display.init()
-
-pygame.mouse.set_visible(0)
-info = pygame.display.Info()
-screen_width = info.current_w
-screen_height = info.current_h
-screen_size = (info.current_w, info.current_h)
-screen_aspect = (float(screen_width) / screen_height)
-screen = pygame.display.set_mode(screen_size)
-
-print "screen size: %d/%d" % (screen_width, screen_height)
-
+class Screen:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.aspect = (float(width) / height)
+        self.handle = pygame.display.set_mode((width, height))
+    
+screen = None
 ws = None
 
 def do_login(ws_):
     ws = ws_
     ws.send("login %s" % (json.dumps({'id': system_id.get(),
                                       'token': get_token(),
-                                      'screen': {'width': screen_width,
-                                                 'height': screen_height}})))
+                                      'screen': {'width': screen.width,
+                                                 'height': screen.height}})))
 
 def load(url):
     img_str = urlopen(url).read()
@@ -42,23 +39,23 @@ def load(url):
     img_aspect = (float(img_width) / img_height)
     print "image size: %d/%d" % (img_width, img_height)
     ratio = 0
-    if math.copysign(1.0, screen_aspect - 1.0) == math.copysign(1.0, img_aspect - 1.0):
-        if screen_aspect > 1:
-            ratio = float(screen_height) / img_height
+    if math.copysign(1.0, screen.aspect - 1.0) == math.copysign(1.0, img_aspect - 1.0):
+        if screen.aspect > 1:
+            ratio = float(screen.height) / img_height
         else:
-            ratio = float(screen_width) / img_width
+            ratio = float(screen.width) / img_width
     else:
-        if screen_aspect > 1:
-            ratio = float(screen_width) / img_width
+        if screen.aspect > 1:
+            ratio = float(screen.width) / img_width
         else:
-            ratio = float(screen_height) / img_height
-    x_offset = (screen_width - (img_width * ratio)) / 2
-    y_offset = (screen_height - (img_height * ratio)) / 2
+            ratio = float(screen.height) / img_height
+    x_offset = (screen.width - (img_width * ratio)) / 2
+    y_offset = (screen.height - (img_height * ratio)) / 2
     scaled_width = int(img_width * ratio)
     scaled_height = int(img_height * ratio)
     img = pygame.transform.scale(img, (scaled_width, scaled_height))
-    screen.fill((0, 0, 0))
-    screen.blit(img, (x_offset, y_offset))
+    screen.handle.fill((0, 0, 0))
+    screen.handle.blit(img, (x_offset, y_offset))
     print "done"
 
 def flip():
@@ -66,6 +63,18 @@ def flip():
 
 def ping():
     pass
+
+def power_down():
+    pygame.display.quit()
+    subprocess.call(['tvservice', '-o'])
+
+def power_up():
+    subprocess.call(['tvservice', '-p'])
+    pygame.init()
+    pygame.mouse.set_visible(0)
+    info = pygame.display.Info()
+    global screen
+    screen = Screen(info.current_w, info.current_h)
 
 def login(status):
     if status == "accepted":
@@ -89,3 +98,5 @@ def set_token(token):
     f = open(token_file, 'w')
     f.write(token)
     f.close()
+
+power_up()
