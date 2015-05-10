248f74de43162d5393f24a68353838c33375c711
Hans Huebner <hans.huebner@gmail.com>
2015-05-10 07:32:24 +0200

    Try migrating to adi (and fail)

--- src/clojure/phrame/storage.clj
+++ src/clojure/phrame/storage.clj
@@ -1,6 +1,7 @@
 (ns phrame.storage
   (:refer-clojure :exclude [swap!])
   (:require [clojure.java.io :as io]
+            [clojure.set :as set]
             [alandipert.enduro :as enduro]
             [adi.core :as adi]
             [phrame.config :as config]))
@@ -22,23 +23,41 @@
                     :google-refresh-token [{:type :string}]
                     :password [{:type :string}]}
 
-             :phrame {:key [{:type :string
-                             :unique :value}]
-                      :name [{:type :string}]
-                      :owner [{:type :ref
-                               :ref {:ns :user}}]
-                      :feed [{:type :ref
-                              :ref {:ns :picasa-album}}]}
-
              :picasa-album {:url [{:type :string
                                    :required true}]
-                            :name [{:type :string
-                                    :required true}]
+                            :title [{:type :string
+                                     :required true}]
                             :id [{:type :long
-                                  :required true}]}})
+                                  :required true
+                                  :unique :value}]}
+
+             :frame {:key [{:type :string
+                            :unique :value}]
+                     :name [{:type :string}]
+                     :token [{:type :string}]
+                     :owner [{:type :ref
+                              :ref {:ns :user}}]
+                     :feed [{:type :ref
+                             :ref {:ns :picasa-album}}]}})
 
 (defn connect [& {:keys [init? install-schema?]}]
   (adi/connect! (:db-url config/config)
                 schema
                 init?
                 install-schema?))
+
+(defn migrate []
+  (let [ds (connect :init? true :install-schema? true)]
+    (adi/insert! ds (vec (map (fn [user]
+                                {:user (set/rename-keys user {:refresh-token :google-refresh-token})})
+                              (vals (:users @data)))))
+    (adi/insert! ds (vec (map (fn [album]
+                                {:picasa-album album})
+                              (apply set/union (map (comp hash-set :album) (vals (:phrames @data)))))))
+    (adi/insert! ds (vec (map (fn [[id phrame]]
+                                (println "id" id "album-id" (:id (:album phrame)))
+                                {:frame {:owner [[:user/email (:owner phrame)]]
+                                         :feed {:id (:id (:album phrame))}
+                                         :key id
+                                         :token (:token phrame)}})
+                              (:phrames @data))))))
